833847892598c289a2fd9e34bd3c301380a81a9f
Verena Lechner <v.m.s.lechner@gmail.com>
2026-05-24 10:58:34 -0600

    revert test changes

--- internal/api/router.go
+++ internal/api/router.go
@@ -36,7 +36,6 @@ func NewRouter(h *Handler, onAuthDone func()) http.Handler {
 		r.Get("/", h.listApplications)
 		r.Post("/", h.createApplication)
 		r.Get("/{id}", h.getApplicationById)
-		r.Get("/{id}/notes", h.getNotesByApplicationID)
 		r.Post("/{id}/correct", h.correctApplication)
 		r.Post("/{id}/reviewed", h.markReviewed)
 		r.Delete("/{id}", h.deleteApplication)
